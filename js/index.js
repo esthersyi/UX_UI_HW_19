f7464b58e8ef04acf26f6dea8d3611d26a90f851
esthersyi <esther.s.yi@du.edu>
2022-10-31 12:17:18 -0600

    jquery

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$( "button").hover(
+    function() {
+        $(this).css("background-color", "#DDE5B6");
+        $(this).css("color", "black");
+        $(this).css("border-color", "black")
+    }, function() {
+        $(this).css("background-color", "#ADC178");
+        $(this).css("color", "black");
+    }
+);
